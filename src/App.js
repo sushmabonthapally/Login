ca0e2224db9b3ac9e8bbc24ab971d800f4d0f6a3
Sumanth Revoori <sumanth@Revanths-MacBook-Pro.local>
2020-11-12 01:05:27 +0530

    login

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React  from 'react';
+import Login from './Login';
+import Details from './employeeList';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Switch>
+      <Route path = "/" exact component = {Login}/>
+      <Route path = "/new" component = {Details}/>
+      </Switch>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
